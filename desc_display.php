113e9c6f172f0ee4685a06074e9ba4f08946bf95
Edward Caissie <edward.caissie@gmail.com>
2013-01-19 14:07:24 -0500

    Over-write file with NoNa code; amend i18n textdomain

--- desc_display.php
+++
@@ -1,6 +0,0 @@
-Enter the color name or hex number.<br />
-For example, the default is "black" or you can enter "#000000" instead.<br />
-You can use a color picker web site such as this one:<br />
-<a href="http://www.colorpicker.com/" target="_blank"><img src="http://www.colorpicker.com/cp/colorpicker.png" border="0"  alt="ColorPicker.com - Quick Online Color Picker Tool"></a>
-<br />(NB: This link will open in a new tab. You can get your color code there, for example, then return to this page and paste it into the text box above.)
-<br />You can also try typing in red, or blue, or purple as well. The choice is yours. Enjoy!
