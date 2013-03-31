834902e85e0bfa85e0ca70024c44705f87bf96c0
Edward Caissie <edward.caissie@gmail.com>
2013-03-31 14:52:10 -0400

    * Code refactoring / formatting and block termination comments added

--- index.php
+++ index.php
@@ -40,43 +40,75 @@
  * @date    January 19, 2013
  * Fully refactored to use NoNa theme version 1.7 (NoNa was originally built
  * from Pinup Meets Grunge).
+ *
+ * @version 2.1
+ * @date    March 31, 2013
+ * Code refactoring / formatting and block termination comments added
  */
 
 get_header(); ?>
+
 <div id="main-blog">
+
     <div id="before-content"></div>
+
     <div id="content">
+
         <?php
-        if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-            <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-                <h2><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'pinup-meets-grunge' ); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-                <div class="post-details">
-                    <?php
-                    printf( __('Posted by %1$s on ', 'pinup-meets-grunge'), get_the_author_meta( 'display_name' ) );
-                    if ( get_the_title() == "" ) { /* for posts without titles - creates a permalink using the post date referencing the post ID */ ?>
-                        <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to post ', 'pinup-meets-grunge' ); the_id(); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a>
+        if ( have_posts() ) {
+
+            while ( have_posts() ) {
+
+                the_post(); ?>
+
+                <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+                    <h2>
+                        <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to', 'pinup-meets-grunge' ); ?> <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+                    </h2>
+
+                    <div class="post-details">
+
+                        <?php
+                        printf( __('Posted by %1$s on ', 'pinup-meets-grunge'),
+                            get_the_author_meta( 'display_name' )
+                        );
+
+                        /** for posts without titles - creates a permalink using the post date referencing the post ID */
+                        if ( get_the_title() == "" ) { ?>
+                            <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php _e( 'Permanent Link to post ', 'pinup-meets-grunge' ); the_id(); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a>
                         <?php } else {
-                        the_time( get_option( 'date_format' ) );
-                    }
-                    echo ' '; comments_popup_link( __( 'with No Comments', 'pinup-meets-grunge' ), __( 'with 1 Comment', 'pinup-meets-grunge' ), __( 'with % Comments', 'pinup-meets-grunge' ), '', __( 'with Comments Closed', 'pinup-meets-grunge' ) );
-                    edit_post_link( __( 'Edit', 'pinup-meets-grunge' ), __( ' | ', 'pinup-meets-grunge' ), __( '', 'pinup-meets-grunge' ) );
-                    _e( '<br />in ', 'pinup-meets-grunge' ); the_category( ', ' ); ?>
-                    <?php
-                    $nona_post_tags = get_the_tags();
-                    if ( $nona_post_tags ) { ?>
+                            the_time( get_option( 'date_format' ) );
+                        } /** End if - no title */
+
+                        echo ' '; comments_popup_link( __( 'with No Comments', 'pinup-meets-grunge' ), __( 'with 1 Comment', 'pinup-meets-grunge' ), __( 'with % Comments', 'pinup-meets-grunge' ), '', __( 'with Comments Closed', 'pinup-meets-grunge' ) );
+                        edit_post_link( __( 'Edit', 'pinup-meets-grunge' ), __( ' | ', 'pinup-meets-grunge' ), __( '', 'pinup-meets-grunge' ) );
+                        _e( '<br />in ', 'pinup-meets-grunge' ); the_category( ', ' );
+
+                        $pmg_post_tags = get_the_tags();
+                        if ( $pmg_post_tags ) { ?>
+                            <br />
+                            <?php the_tags( __( 'as ', 'pinup-meets-grunge' ), ', ', '' );
+                        } /** End if - has post tags */ ?>
+
                         <br />
-                        <?php the_tags( __( 'as ', 'pinup-meets-grunge' ), ', ', '' ); } ?>
-                    <br />
-                </div><!-- .post-details -->
-                <?php
-                if ( has_post_thumbnail() ) {
-                    the_post_thumbnail( 'thumbnail', array( 'class' => 'alignleft' ) );
-                }
-                the_content( __( 'Read more... ', 'pinup-meets-grunge' ) ); ?>
-                <div class="clear"></div><!-- For inserted media at the end of the post -->
-                <?php wp_link_pages( array( 'before' => '<p><strong>' . __( 'Pages: ', 'pinup-meets-grunge') . '</strong>', 'after' => '</p>', 'next_or_number' => 'number' ) ); ?>
-            </div><!-- .post #post-ID -->
-            <?php endwhile; ?>
+
+                    </div><!-- .post-details -->
+
+                    <?php
+                    if ( has_post_thumbnail() ) {
+                        the_post_thumbnail( 'thumbnail', array( 'class' => 'alignleft' ) );
+                    } /** End if - has post thumbnail */
+                    the_content( __( 'Read more... ', 'pinup-meets-grunge' ) ); ?>
+
+                    <div class="clear"></div><!-- For inserted media at the end of the post -->
+
+                    <?php wp_link_pages( array( 'before' => '<p><strong>' . __( 'Pages: ', 'pinup-meets-grunge') . '</strong>', 'after' => '</p>', 'next_or_number' => 'number' ) ); ?>
+
+                </div><!-- .post #post-ID -->
+
+            <?php } /** End while - have posts */ ?>
+
             <div id="nav-global" class="navigation">
                 <div class="left">
                     <?php next_posts_link( __( '&laquo; Previous entries ', 'pinup-meets-grunge' ) ); ?>
@@ -85,15 +117,27 @@ get_header(); ?>
                     <?php previous_posts_link( __( ' Next entries &raquo;', 'pinup-meets-grunge' ) ); ?>
                 </div>
             </div><!-- .navigation -->
+
             <div class="clear"></div>
-            <?php else : ?>
-            <h2><?php printf( __( 'Search Results for: %s', 'pinup-meets-grunge' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h2>
+
+        <?php } else { ?>
+
+            <h2>
+                <?php printf( __( 'Search Results for: %s', 'pinup-meets-grunge' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?>
+            </h2>
             <p><?php _e( 'Sorry, but you are looking for something that is not here.', 'pinup-meets-grunge' ); ?></p>
+
             <?php
             get_search_form();
-        endif; ?>
+
+        } /** End if - have posts */ ?>
+
     </div><!-- #content -->
+
     <div id="after-content"></div>
+
 </div><!-- #main-blog -->
-<?php get_sidebar();
+
+<?php
+get_sidebar();
 get_footer();
--- readme.txt
+++ readme.txt
@@ -1,2 +1,4 @@
 === Pinup Meets Grunge ===
 Refactored and released as version 2.0 on January 19, 2013.
+
+Although the theme is functional it may not be fully supported going forward.
